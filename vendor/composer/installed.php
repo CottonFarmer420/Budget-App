3b92c22b4f14ac0dc42a35c9b003f595e1182748
Webspace mscharler.swpp.mscharler.eu <webspace@mscharler.swpp.mscharler.eu>
2025-03-04 20:23:44 +0100

    update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'v12.0.1',
-        'version' => '12.0.1.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6ebdef0cd3a5a235f5eab27aa9b999749e63f55b',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -386,9 +386,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'v12.0.1',
-            'version' => '12.0.1.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6ebdef0cd3a5a235f5eab27aa9b999749e63f55b',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
